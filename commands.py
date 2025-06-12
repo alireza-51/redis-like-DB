db962b572f60e1f80b8db60d367213f784a6ccc8
alireza-51 <alirezazarei51@gmail.com>
2025-06-12 12:58:06 +0330

    add INCR, INCRBY commands

--- commands.py
+++ commands.py
@@ -44,3 +44,44 @@ class DelCommand(Command):
             return RESP.encode_simple_string('+DEL')
         except KeyError:
             return RESP.encode_error(f"'{key}' key not found.")
+
+
+class IncrCommand(Command):
+    def execute(self, args:List[bytes]) -> bytes:
+        
+        if len(args) != 1:
+            return RESP.encode_error("wrong number of arguments for 'INCR'")
+        
+        key = args[0].decode()
+            
+        value = self._data_store.get(key)
+        try:
+            value = int(value) if value is not None else 0
+        except ValueError:
+            return RESP.encode_error(f"value is not an integer or out of range")
+        value += 1
+        self._data_store.set(key, str(value))
+        return RESP.encode_integer(value)
+
+
+class IncrByCommand(Command):
+    def execute(self, args:List[bytes]) -> bytes:
+        
+        if len(args) != 2:
+            return RESP.encode_error("wrong number of arguments for 'INCRBY'")
+        
+        key = args[0].decode()
+        delta_value = args[1].decode()
+        current_value = self._data_store.get(key)
+        try:
+            delta_value = int(delta_value)
+        except ValueError:
+            return RESP.encode_error(f"'{delta_value}' is not an integer.")
+        try:
+            current_value = int(current_value) if current_value is not None else 0
+        except ValueError:
+            return RESP.encode_error(f"current value for '{key}' is not an integer.")
+        
+        value = current_value + delta_value
+        self._data_store.set(key, str(value))
+        return RESP.encode_integer(value)
